8eb448a7637355ff8e5f5f328419b0c3370b7a8c
ElectricGamer100 <MCAnonymousModders@gmail.com>
2015-10-19 18:32:03 -0400

    bruh im stupid

--- BlastFurnace.js
+++ BlastFurnace.js
@@ -2,7 +2,7 @@ var structureOneX = {x1: 0, x2: 0, x3: 0, x4: 0, x5: 0, x6: 0, x7: 0, x8: 0, x9:
 var structureOneY = {y1: 0, y2: 0, y3: 0, y4: 0, y5: 0, y6: 0, y7: 0, y8: 0, y9:0};
 var structureOneZ = {z1: 0, z2: 0, z3: 0, z4: 0, z5: 0, z6: 0, z7: 0, z8: 0, z9:0};
 
-var structureOneConfirmed = false:
+var structureOneConfirmed = false;
 
 function useItem(x,y,z,i,b,s,id,bd){
   if(i == 0 && b == 1 && Level.getTile(x+1,y,z) == 1 && Level.getTile(x-1,y,z) == 1 && Level.getTile(x+1,y,z+1) == 1 && Level.getTile(x-1,y,z+1) == 1 && Level.getTile(x,y,z+1) == 1 && Level.getTile(x+1,y,z+2) == 1 && Level.getTile(x-1,y,z+2) == 1 && Level.getTile(x,y,z+2) == 1){
