9e27aad4246e1f23627d135cc23e3f77e6b6f5ad
ElectricGamer100 <MCAnonymousModders@gmail.com>
2015-10-19 17:17:32 -0400

    update

--- BlastFurnace.js
+++ BlastFurnace.js
@@ -1,1 +1,10 @@
+var structureOneX = {x1: 0, x2: 0, x3: 0, x4: 0, x5: 0, x6: 0, x7: 0, x8: 0, x9:0};
+var structureOneY = {y1: 0, y2: 0, y3: 0, y4: 0, y5: 0, y6: 0, y7: 0, y8: 0, y9:0};
+var structureOneZ = {z1: 0, z2: 0, z3: 0, z4: 0, z5: 0, z6: 0, z7: 0, z8: 0, z9:0};
+
+var structureOneConfirmed = false:
+
+function useItem(x,y,z,i,b,s,id,bd){
+  
+}
 
