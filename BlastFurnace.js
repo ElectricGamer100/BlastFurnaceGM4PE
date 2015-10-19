5e6b640d1d4be172c16e86f61edffbc5f18b1c54
ElectricGamer100 <MCAnonymousModders@gmail.com>
2015-10-19 18:20:04 -0400

    damn

--- BlastFurnace.js
+++ BlastFurnace.js
@@ -5,6 +5,38 @@ var structureOneZ = {z1: 0, z2: 0, z3: 0, z4: 0, z5: 0, z6: 0, z7: 0, z8: 0, z9:
 var structureOneConfirmed = false:
 
 function useItem(x,y,z,i,b,s,id,bd){
-  
+  if(i == 0 && b == 1 && Level.getTile(x+1,y,z) == 1 && Level.getTile(x-1,y,z) == 1 && Level.getTile(x+1,y,z+1) == 1 && Level.getTile(x-1,y,z+1) == 1 && Level.getTile(x,y,z+1) == 1 && Level.getTile(x+1,y,z+2) == 1 && Level.getTile(x-1,y,z+2) == 1 && Level.getTile(x,y,z+2) == 1){
+    structureOneConfirmed = true;
+    structureOneX.x1 = x;
+    structureOneX.x2 = x+1;
+    structureOneX.x3 = x-1;
+    structureOneX.x4 = x+1;
+    structureOneX.x5 = x-1;
+    structureOneX.x6 = x;
+    structureOneX.x7 = x+1;
+    structureOneX.x9 = x-1;
+    structureOneX.x9 = x;
+    
+    structureOneY.y1 = y;
+    structureOneY.y2 = y;
+    structureOneY.y3 = y;
+    structureOneY.y4 = y;
+    structureOneY.y5 = y;
+    structureOneY.y6 = y;
+    structureOneY.y7 = y;
+    structureOneY.y8 = y;
+    structureOneY.y9 = y;
+    
+    structureOneZ.z1 = z;
+    structureOneZ.z2 = z;
+    structureOneZ.z3 = z;
+    structureOneZ.z4 = z+1;
+    structureOneZ.z5 = z+1;
+    structureOneZ.z6 = z+1;
+    structureOneZ.z7 = z+2;
+    structureOneZ.z8 = z+2;
+    structureOneZ.z9 = z+2;
+    clientMessage("Yay it works");
+  }
 }
 
