634f602b17987cbfd1766684db33b5246f36a3ab
JustinGamer <MCAnonymousModders@gmail.com>
2015-10-20 13:19:41 -0400

    AddedShit

--- BlastFurnace.js
+++ BlastFurnace.js
@@ -1,6 +1,8 @@
 var structureOneX = {x1: 0, x2: 0, x3: 0, x4: 0, x5: 0, x6: 0, x7: 0, x8: 0, x9:0};
 var structureOneY = {y1: 0, y2: 0, y3: 0, y4: 0, y5: 0, y6: 0, y7: 0, y8: 0, y9:0};
 var structureOneZ = {z1: 0, z2: 0, z3: 0, z4: 0, z5: 0, z6: 0, z7: 0, z8: 0, z9:0};
+var structureOneIronCount = 0;
+var structureOneGoldCount = 0;
 
 var structureOneConfirmed = false;
 
@@ -51,6 +53,8 @@ function leaveGame(){
   ModPE.saveData("structureOneX",structureOneX);
   ModPE.saveData("structureOneY",structureOneY);
   ModPE.saveData("structureOneZ",structureOneZ);
+  ModPE.saveData("structureOneIronCount", structureOneIronCOunt);
+  ModPE.saveData("structureOneGoldCount", structureOneGoldCOunt);
 }
 
 function newLevel(){
@@ -58,4 +62,35 @@ function newLevel(){
   ModPE.readData("structureOneX");
   ModPE.readData("structureOneY");
   ModPE.readData("structureOneZ");
+  ModPE.readData("structureOneIronCount");
+  ModPE.readData("structureOneGoldCount");
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
